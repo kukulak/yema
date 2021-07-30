c7e43ac053a339d2a5d1bb26fd8b53681cf3973c
Christian Valderrama <kukulak.cero@gmail.com>
2021-07-30 11:36:36 -0500

    device-detect

--- components/card/card.component.jsx
+++ components/card/card.component.jsx
@@ -5,6 +5,13 @@ import CardImage from './cardImage.component';
 import CardInfo from './cardInfo.component';
 import CardFecha from './cardFecha.component';
 import Tilt from 'react-vanilla-tilt'
+
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
  
   <div>
     You can put whatever you want inside this
@@ -14,13 +21,33 @@ import Tilt from 'react-vanilla-tilt'
 export default function Card({date, image, link, red, name}) {
     return (
         
-        <Tilt className={styles.tilt} options={{ "glare-prerender": false, "max-glare": 1, glare: true, scale: 12, max: 25 }}>
-            <div className={styles.card}>
-                <CardFecha date={date} />
-                <CardImage image={image}/>
-                <CardInfo date={date} link={link} name={name} red={red}/>
+      <>
+        <BrowserView>
 
-            </div>
-        </Tilt>
+          <Tilt className={styles.tilt} options={{ "glare-prerender": false, "max-glare": 1, glare: true, scale: 12, max: 25 }}>
+              <div className={styles.card}>
+                  <CardFecha date={date} />
+                  <CardImage image={image}/>
+                  <CardInfo date={date} link={link} name={name} red={red}/>
+
+              </div>
+          </Tilt>
+
+        </BrowserView>
+
+        <MobileView>
+
+  
+        <div className={styles.card}>
+            <CardFecha date={date} />
+            <CardImage image={image}/>
+            <CardInfo date={date} link={link} name={name} red={red}/>
+
+        </div>
+  
+        </MobileView>
+        
+      </>
+
     )
   }
